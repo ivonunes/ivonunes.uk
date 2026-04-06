51f81441106be4971c77fd3cdccb056a7745f724
Ivo Nunes <ivo@nunes.uk>
2026-04-06 13:23:43 +0100

    Add pagination

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -10,6 +10,14 @@ module.exports = {
     postName: (data) => getPostName(data.title, data.rawBody),
     indexExcerpt: (data) => getIndexExcerpt(data.rawBody, data),
     indexHasMore: (data) => hasIndexExcerptMore(data.rawBody, data),
-    syndicationUrls: (data) => getSyndicationUrls(data)
+    syndicationUrls: (data) => getSyndicationUrls(data),
+    prevPost: (data) => {
+      const idx = data.collections.posts.findIndex(p => p.url === data.page.url);
+      return idx >= 0 && idx < data.collections.posts.length - 1 ? data.collections.posts[idx + 1] : null;
+    },
+    nextPost: (data) => {
+      const idx = data.collections.posts.findIndex(p => p.url === data.page.url);
+      return idx > 0 ? data.collections.posts[idx - 1] : null;
+    }
   }
 };
